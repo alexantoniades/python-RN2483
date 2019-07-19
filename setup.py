0a8b688491bff6578fd299c620fe5003e63bba1e
Alexandros Antoniades <antoniades.alex@gmail.com>
2019-07-19 21:42:23 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="rn2483",
     version="0.1",
     license="Apache License, Version 2.0",
-    description="en2483 library",
+    description="rn2483 library",
     author="Alexandros Antoniades",
     author_email="alex.rogue.antoniades@gmail.com",
     url="https://github.com/alexantoniades/python-rn2483",
