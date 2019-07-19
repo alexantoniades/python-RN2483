4a214c409e02c1021a44923c7ad434cbda7ecbbf
Alexandros Antoniades <antoniades.alex@gmail.com>
2019-07-19 13:41:10 +0000

    Updated rn2483.py

--- rn2483.py
+++ rn2483.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python
 
-# Apache License 2.0
-# Copyright (c) 2019 Alexandros Antoniades
-
-# Author: Alexandros Antoniades
-# Date: 16/07/2019
-# Description: Python library for RN2483 LoRaWAN transceiver
-# This implementation is meant to be used with a Micropython implementation on Raspberry Pi, ESP32, PyBoard etc.
-# Version: 0.1
-
-# RN2483 documentation: https://ww1.microchip.com/downloads/en/DeviceDoc/40001784B.pdf
+LICENSE = """
+Apache License 2.0
+Copyright (c) 2019 Alexandros Antoniades
+"""
+AUTHOR = "Alexandros Antoniades"
+DESCRIPTION = """
+            [ RN2483 Library ]
+Python library for RN2483 LoRaWAN transceiver
+This implementation is meant to be used with a 
+Micropython implementation on Raspberry Pi, ESP32, 
+PyBoard etc.
+[------------------------------------------------]"""
+VERSION = "0.1"
+DOC = { 
+    "UserGuide": "https://ww1.microchip.com/downloads/en/DeviceDoc/40001784B.pdf",
+    "Datasheet": "http://ww1.microchip.com/downloads/en/devicedoc/50002346c.pdf"
+}
+GIT = "https://github.com/alexantoniades/python-RN2483"
 
 from machine import UART, ADC, Pin, PWM
 import time
@@ -20,8 +28,8 @@ class RN2483:
     COMMANDS = {
         # System commands
         "SYSTEM": {
-            "SLEEP": "sys sleep {duration}",
-            "RESET": "sys reset",
+            "SLEEP": "sys sleep {duration}",                    # duration = decimal [milliseconds]
+            "RESET": "sys reset",                               
             "FACTORY_RESET": "sys factoryRESET",
             "ERASEFW": "sys eraseFW",
             "VERSION": "sys get ver",
@@ -29,87 +37,93 @@ class RN2483:
             "HWEUI": "sys get hweui",
             # Memory and I/O
             "NVM": {
-                    "GET": "sys get nvm {address}",
-                    "SET": "sys set nvm {address} {value}"
+                    "GET": "sys get nvm {address}",             # address = Hexadecimal
+                    "SET": "sys set nvm {address} {value}"      # address, value = Hexadecimal
             },
             "PIN": {
-                "GPIO0": "sys set pindig GPIO0 {state}",
-                "GPIO1": "sys set pindig GPIO1 {state}",
-                "GPIO2": "sys set pindig GPIO2 {state}",
-                "GPIO3": "sys set pindig GPIO3 {state}",
-                "GPIO4": "sys set pindig GPIO4 {state}",
-                "GPIO5": "sys set pindig GPIO5 {state}",
-                "GPIO6": "sys set pindig GPIO6 {state}",
-                "GPIO7": "sys set pindig GPIO7 {state}",
-                "GPIO8": "sys set pindig GPIO8 {state}",
-                "GPIO9": "sys set pindig GPIO9 {state}",
-                "GPIO10": "sys set pindig GPIO10 {state}",
-                "GPIO11": "sys set pindig GPIO11 {state}",
-                "GPIO12": "sys set pindig GPIO12 {state}",
-                "GPIO13": "sys set pindig GPIO13 {state}",
-                "GPIO14": "sys set pindig GPIO14 {state}",
-                "UART_CTS": "sys set pindig UART_CTS {state}",
-                "UART_RTS": "sys set pindig UART_RTS {state}"
+                "GPIO0": "sys set pindig GPIO0 {state}",        # state = 1 or 0 [HIGH or LOW]
+                "GPIO1": "sys set pindig GPIO1 {state}",        # state = 1 or 0 [HIGH or LOW]
+                "GPIO2": "sys set pindig GPIO2 {state}",        # state = 1 or 0 [HIGH or LOW]
+                "GPIO3": "sys set pindig GPIO3 {state}",        # state = 1 or 0 [HIGH or LOW]
+                "GPIO4": "sys set pindig GPIO4 {state}",        # state = 1 or 0 [HIGH or LOW]
+                "GPIO5": "sys set pindig GPIO5 {state}",        # state = 1 or 0 [HIGH or LOW]
+                "GPIO6": "sys set pindig GPIO6 {state}",        # state = 1 or 0 [HIGH or LOW]
+                "GPIO7": "sys set pindig GPIO7 {state}",        # state = 1 or 0 [HIGH or LOW]
+                "GPIO8": "sys set pindig GPIO8 {state}",        # state = 1 or 0 [HIGH or LOW]
+                "GPIO9": "sys set pindig GPIO9 {state}",        # state = 1 or 0 [HIGH or LOW]
+                "GPIO10": "sys set pindig GPIO10 {state}",      # state = 1 or 0 [HIGH or LOW]
+                "GPIO11": "sys set pindig GPIO11 {state}",      # state = 1 or 0 [HIGH or LOW]
+                "GPIO12": "sys set pindig GPIO12 {state}",      # state = 1 or 0 [HIGH or LOW]
+                "GPIO13": "sys set pindig GPIO13 {state}",      # state = 1 or 0 [HIGH or LOW]
+                "GPIO14": "sys set pindig GPIO14 {state}",      # state = 1 or 0 [HIGH or LOW]
+                "UART_CTS": "sys set pindig UART_CTS {state}",  # state = 1 or 0 [HIGH or LOW]
+                "UART_RTS": "sys set pindig UART_RTS {state}"   # state = 1 or 0 [HIGH or LOW]
             }
         },
         # LoRaWAN interface commands
         "MAC": {
-            "RESET_BAND": "mac reset {band}",
-            "TX": "mac tx {type} {portnumber} {data}",
+            "RESET_BAND": {
+                "868": "mac reset 868",
+                "433": "mac reset 433"
+            },
+            "TX": {
+                "CONFIRMED": "mac tx cnf {portnumber} {data}",  # portnumber = decimal [1-223] 
+                "UNCONFIRMED": "mac tx uncnf {portnumber} {data}" # data = Hexadecimal
+            },
             "SAVE": "mac save",
             "PAUSE": "mac pause",
             "RESUME": "mac resume",
             # LoRaWAN settings
             "DEVADDR": {
                     "GET": "mac get devaddr",
-                    "SET": "mac set devaddr {address}"
+                    "SET": "mac set devaddr {address}"          # address = Hexadecimal
             },
             "DEVEUI": {
                     "GET": "mac get deveui",
-                    "SET": "mac set deveui {deveui}"
+                    "SET": "mac set deveui {deveui}"            # deveui = 8-byte Hexadecimal        
             },
             "APPEUI": {
                     "GET": "mac get appeui",
-                    "SET": "mac set appeui {appeui}"
+                    "SET": "mac set appeui {appeui}"            # appeui = 8-byte Hexadecimal
             },
             "DATARATE": {
                     "GET": "mac get dr",
-                    "SET": "mac set dr {datarate}"
+                    "SET": "mac set dr {datarate}"              # datarate = decimal [0-7]
             },
             "BAND": "mac get band",
             "POWER_INDEX": {
                     "GET": "mac get pwridx",
-                    "SET": "mac set pwridx {power_index}"
+                    "SET": "mac set pwridx {powerindex}"       # powerindex = decimal [0-5]
             },
             "ADAPTIVE_DATARATE": {
                     "GET": "mac get adr",
-                    "SET": "mac set adr {datarate}"
+                    "SET": "mac set adr {state}"                # state = on or off
             },
             "RETX": {
                     "GET": "mac get retx",
-                    "SET": "mac set retx {retx}"
+                    "SET": "mac set retx {retx}"                # retx = decimal [0-255]
             },
             "RXDELAY1": {
                     "GET": "mac get rxdelay1",
-                    "SET": "mac set rxdelay1 {delay}",
+                    "SET": "mac set rxdelay1 {delay}"           # delay = decimal [milliseconds][0-65535]
             },
             "RXDELAY2": "mac get rxdelay2",
             "AUTO_REPLY": {
                     "GET": "mac get ar",
-                    "SET": "mac set ar {reply}",
+                    "SET": "mac set ar {reply}"                 # reply = on or off
             },
             "RX2": {
-                    "GET": "mac get rx2 {band}",
-                    "SET": "mac set rx2 {datarate} {frequency}",
+                    "GET": "mac get rx2 {band}",                # decimal [868 or 433]
+                    "SET": "mac set rx2 {datarate} {frequency}" # datarate, frequency = decimal [0-7], [863000000-870000000] or [433050000-434790000]
             },
             "DYCLEPS": "mac get dcycleps",
             "MARGIN": "mac get mrgn",
             "GATEWAY_NUMBER": "mac get gwnb",
             "STATUS": "mac get status",
-            "NWKSKEYSET": "mac set nwkskey {key}",
-            "APPSKEYSET": "mac set appskey {key}",
-            "BAT": "mac set bat {level}",
-            "LINK_CKECK": "mac set linkchk {check}",
+            "NWKSKEYSET": "mac set nwkskey {key}",              # key = 16-byte Hexadecimal
+            "APPSKEYSET": "mac set appskey {key}",              # key = 16-bute Hexadecimal
+            "BAT": "mac set bat {level}",                       # level = decimal [0-255]
+            "LINK_CKECK": "mac set linkchk {check}"             # check = decimal [0-65535]
         },
         # Radio interface
         "RADIO": {
@@ -202,58 +216,73 @@ class RN2483:
     # Pass and Execute command to device, return device response
     def execute(self, command):
         self.serial.write(bytes(str(command) + "\r\n", "utf-8"))
-        response = str((self.serial.readline()).decode("utf-8"))
+        response = (self.serial.readline()).decode("utf-8")
         if self.debug:
-            print("Execute: {command} Response: {response}\r\n").format(command=str(command), response=response)
+            print("Execute: {command} Response: {response}\r\n".format(command=command, response=response))
+            
         return(response)
     
     # Get device version
     def version(self):
-        return(execute(self.COMMANDS["SYSTEM"]["VERSION"]))
+        return(self.execute(self.COMMANDS["SYSTEM"]["VERSION"]))
     
     # Get device voltage level
     def voltage(self):
-        return(execute(self.COMMANDS["SYSTEM"]["VOLTAGE"]))
+        return(self.execute(self.COMMANDS["SYSTEM"]["VOLTAGE"]))
     
     # Get device hardware EUI
     def hardwareEUI(self):
-        return(execute(self.COMMANDS["SYSTEM"]["HWEUI"]))
+        return(self.execute(self.COMMANDS["SYSTEM"]["HWEUI"]))
     
     # Get value at specific address (address in HEX)
     def getValueAtAddress(self, address):
-        return(execute(self.COMMANDS["SYSTEM"]["NVM"]["GET"].format(address=str(address))))
+        return(self.execute(self.COMMANDS["SYSTEM"]["NVM"]["GET"].format(address=str(address))))
     
     # Set a value at specific memory address (value in HEX)
     def setValueAtAddress(self, address, value):
-        return(execute(self.COMMANDS["SYSTEM"]["NVM"]["SET"].format(address=str(address), value=str(value))))
+        return(self.execute(self.COMMANDS["SYSTEM"]["NVM"]["SET"].format(address=str(address), value=str(value))))
         
-    def setPin(self, pin, state):
-        return(execute())
-
     # Set device to sleep for specific duration (duration in milliseconds)
     def sleep(self, duration):
-        return(execute(self.COMMANDS["SYSTEM"]["SLEEP"].format(duration=str(duration))))
+        return(self.execute(self.COMMANDS["SYSTEM"]["SLEEP"].format(duration=str(duration))))
     
     # Set device to reset (ON OFF)
     def reset(self):
-        return(execute(self.COMMANDS["SYSTEM"]["RESET"]))
+        return(self.execute(self.COMMANDS["SYSTEM"]["RESET"]))
     
     # Factory reset device
     def factoryReset(self):
-        return(execute(self.COMMANDS["SYSTEM"]["FACTORY_RESET"]))
+        return(self.execute(self.COMMANDS["SYSTEM"]["FACTORY_RESET"]))
     
     # Change state of GPIO pin: 1 = UP, 0 = DOWN
     def setPin(self, pin, state):
-        return(execute(self.COMMANDS["SYSTEM"]["PIN"][str(pin)].format(state=str(state))))
+        return(self.execute(self.COMMANDS["SYSTEM"]["PIN"][str(pin)].format(state=str(state))))
         
+    def report(self):
+        return("""
+{description}
+
+By {author}
+Version: {version}
+Github repository can be found at {github}
+
+{license}
+        """.format(description=DESCRIPTION, author=AUTHOR, version=VERSION, github=GIT, license=LICENSE))
+        
+    # Setup LoRaWAN client
+    def setup(self):
+        return 0
     
     
 def main():
     
-    uart1 = UART(PORT, BAUDRATE)
-    uart1.init(BAUDRATE, bits=8, parity=None, stop=1)
+    PORT = 1
+    BAUDRATE = 57600
     
-    device = RN2483(serial=uart1, debug=True)
+    uart = UART(PORT, BAUDRATE)
+    uart.init(BAUDRATE, bits=8, parity=None, stop=1)
+    
+    device = RN2483(serial=uart, debug=True)
     
     
     
