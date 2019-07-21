56c370d7c31be9c4c6366a7061a91be048cde87a
Alexandros Antoniades <alex.rogue.antoniades@gmail.com>
2019-07-21 20:14:06 +0000

    Refined quality of code 9.48/10 pylint score

--- rn2483.py
+++ rn2483.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python
-
-#from machine import UART, ADC, Pin, PWM
+''' RN2483 Library File '''
 #import time
 #import sys
 #TODO: Platform recognition and timestamps
 import serial
-
 LICENSE = """
 Apache License 2.0
 Copyright (c) 2019 Alexandros Antoniades
@@ -17,24 +15,20 @@ DESCRIPTION = """
     This implementation is meant to be used on Raspberry Pi.
     [-------------------------------------------------------]"""
 VERSION = "0.1"
-DOC = { 
+DOC = {
     "UserGuide": "https://ww1.microchip.com/downloads/en/DeviceDoc/40001784B.pdf",
     "Datasheet": "http://ww1.microchip.com/downloads/en/devicedoc/50002346c.pdf"
 }
 GIT = "https://github.com/alexantoniades/python-RN2483"
-
-
-
 PORT = "/dev/tty"
 BAUDRATE = 57600
-
 class RN2483:
     # Commands for RN2483 and RN2903 can be found in the product user guide by Microchip
     COMMANDS = {
         # System commands
         "SYSTEM": {
             "SLEEP": "sys sleep {duration}",                    # duration = decimal [milliseconds]
-            "RESET": "sys reset",                               
+            "RESET": "sys reset",
             "FACTORY_RESET": "sys factoryRESET",
             "ERASEFW": "sys eraseFW",
             "VERSION": "sys get ver",
@@ -42,8 +36,8 @@ class RN2483:
             "HWEUI": "sys get hweui",
             # Memory and I/O
             "NVM": {
-                    "GET": "sys get nvm {address}",             # address = Hexadecimal
-                    "SET": "sys set nvm {address} {value}"      # address, value = Hexadecimal
+                "GET": "sys get nvm {address}",             # address = Hexadecimal
+                "SET": "sys set nvm {address} {value}"      # address, value = Hexadecimal
             },
             "PIN": {
                 "GPIO0": "sys set pindig GPIO0 {state}",        # state = 1 or 0 [HIGH or LOW]
@@ -72,7 +66,7 @@ class RN2483:
                 "433": "mac reset 433"
             },
             "TX": {
-                "CONFIRMED": "mac tx cnf {portnumber} {data}",  # portnumber = decimal [1-223] 
+                "CONFIRMED": "mac tx cnf {portnumber} {data}",  # portnumber = decimal [1-223]
                 "UNCONFIRMED": "mac tx uncnf {portnumber} {data}" # data = Hexadecimal
             },
             "SAVE": "mac save",
@@ -80,46 +74,46 @@ class RN2483:
             "RESUME": "mac resume",
             # LoRaWAN settings
             "DEVADDR": {
-                    "GET": "mac get devaddr",
-                    "SET": "mac set devaddr {address}"          # address = Hexadecimal
+                "GET": "mac get devaddr",
+                "SET": "mac set devaddr {address}"          # address = Hexadecimal
             },
             "DEVEUI": {
-                    "GET": "mac get deveui",
-                    "SET": "mac set deveui {deveui}"            # deveui = 8-byte Hexadecimal        
+                "GET": "mac get deveui",
+                "SET": "mac set deveui {deveui}"            # deveui = 8-byte Hexadecimal
             },
             "APPEUI": {
-                    "GET": "mac get appeui",
-                    "SET": "mac set appeui {appeui}"            # appeui = 8-byte Hexadecimal
+                "GET": "mac get appeui",
+                "SET": "mac set appeui {appeui}"            # appeui = 8-byte Hexadecimal
             },
             "DATARATE": {
-                    "GET": "mac get dr",
-                    "SET": "mac set dr {datarate}"              # datarate = decimal [0-7]
+                "GET": "mac get dr",
+                "SET": "mac set dr {datarate}"              # datarate = decimal [0-7]
             },
             "BAND": "mac get band",
             "POWER_INDEX": {
-                    "GET": "mac get pwridx",
-                    "SET": "mac set pwridx {powerindex}"       # powerindex = decimal [0-5]
+                "GET": "mac get pwridx",
+                "SET": "mac set pwridx {powerindex}"       # powerindex = decimal [0-5]
             },
             "ADAPTIVE_DATARATE": {
-                    "GET": "mac get adr",
-                    "SET": "mac set adr {state}"                # state = on or off
+                "GET": "mac get adr",
+                "SET": "mac set adr {state}"                # state = on or off
             },
             "RETX": {
-                    "GET": "mac get retx",
-                    "SET": "mac set retx {retx}"                # retx = decimal [0-255]
+                "GET": "mac get retx",
+                "SET": "mac set retx {retx}"                # retx = decimal [0-255]
             },
             "RXDELAY1": {
-                    "GET": "mac get rxdelay1",
-                    "SET": "mac set rxdelay1 {delay}"           # delay = decimal [milliseconds][0-65535]
+                "GET": "mac get rxdelay1",
+                "SET": "mac set rxdelay1 {delay}"           # delay = decimal [milliseconds][0-65535]
             },
             "RXDELAY2": "mac get rxdelay2",
             "AUTO_REPLY": {
-                    "GET": "mac get ar",
-                    "SET": "mac set ar {reply}"                 # reply = on or off
+                "GET": "mac get ar",
+                "SET": "mac set ar {reply}"                 # reply = on or off
             },
             "RX2": {
-                    "GET": "mac get rx2 {band}",                # decimal [868 or 433]
-                    "SET": "mac set rx2 {datarate} {frequency}" # datarate, frequency = decimal [0-7], [863000000-870000000] or [433050000-434790000]
+                "GET": "mac get rx2 {band}",                # decimal [868 or 433]
+                "SET": "mac set rx2 {datarate} {frequency}" # datarate, frequency = decimal [0-7], [863000000-870000000] or [433050000-434790000]
             },
             "DYCLEPS": "mac get dcycleps",
             "MARGIN": "mac get mrgn",
@@ -131,7 +125,6 @@ class RN2483:
             "APPKEY": "mac set appkey {key}",                # key = 16-byte Hexadecimal
             "BAT": "mac set bat {level}",                       # level = decimal [0-255]
             "LINK_CKECK": "mac set linkchk {check}"             # check = decimal [0-65535]
-
         },
         # Radio interface
         "RADIO": {
@@ -205,16 +198,15 @@ class RN2483:
             }
         }
     }
-    
     def __init__(self, serial=None, debug=False):
+        ''' Class init, check if serial connection is open '''
         self.serial = serial
         self.debug = debug
-        
         if self.serial == None:
             raise ValueError("Invalid serial connection")
-        
-    def report(self):
-        return("""
+    def info(self):
+        ''' Returns Library info '''
+        return """
     {description}
     
     By {author}
@@ -222,134 +214,111 @@ class RN2483:
     Github repository can be found at {github}
     
     {license}
-        """.format(description=DESCRIPTION, author=AUTHOR, version=VERSION, github=GIT, license=LICENSE))    
-    
+        """.format(description=DESCRIPTION, author=AUTHOR, version=VERSION, github=GIT, license=LICENSE)
     # Get serial connection
-    def serialConnection(self):
-        return(self.serial)
-    
+    def serial_connection(self):
+        ''' Serial connection info '''
+        return self.serial
     # Close serial connection
-    def closeConnection(self):
-        return(self.serial.deinit())
-    
+    def close_connection(self):
+        ''' Close serial connection '''
+        return self.serial.deinit()
     # Pass and Execute command to device, return device response
     def execute(self, command):
+        ''' Passes and Executes command to device, returns devices response '''
         self.serial.write(bytes(str(command) + "\r\n", "utf-8"))
         response = (self.serial.readline()).decode("utf-8")
         if self.debug:
-            print("Execute: {command} Response: {response}\r\n".format(command=command, response=response))
-            
-        return(response)
-    
+            print "Execute: {command} Response: {response}\r\n".format(command=command, response=response)
+        return response
     # Get device version
     def version(self):
-        return(self.execute(self.COMMANDS["SYSTEM"]["VERSION"]))
-    
+        ''' Returns RN2483 version '''
+        return self.execute(self.COMMANDS["SYSTEM"]["VERSION"])
     # Get device voltage level
     def voltage(self):
-        return(self.execute(self.COMMANDS["SYSTEM"]["VOLTAGE"]))
-    
+        ''' Returns RN2483 Voltage '''
+        return self.execute(self.COMMANDS["SYSTEM"]["VOLTAGE"])
     # Get device hardware EUI
-    def hardwareEUI(self):
-        return(self.execute(self.COMMANDS["SYSTEM"]["HWEUI"]))
-    
+    def hardware_eui(self):
+        ''' Returns RN2483 Hardware EUI '''
+        return self.execute(self.COMMANDS["SYSTEM"]["HWEUI"])
     # Get value at specific address (address in HEX)
-    def getValueAtAddress(self, address):
-        return(self.execute(self.COMMANDS["SYSTEM"]["NVM"]["GET"].format(address=str(address))))
-    
+    def get_value_at_address(self, address):
+        ''' Returns value at memory address '''
+        return self.execute(self.COMMANDS["SYSTEM"]["NVM"]["GET"].format(address=str(address)))
     # Set a value at specific memory address (value in HEX)
-    def setValueAtAddress(self, address, value):
-        return(self.execute(self.COMMANDS["SYSTEM"]["NVM"]["SET"].format(address=str(address), value=str(value))))
-        
+    def set_value_at_address(self, address, value):
+        ''' Sets value at memory address '''
+        return self.execute(self.COMMANDS["SYSTEM"]["NVM"]["SET"].format(address=str(address), value=str(value)))
     # Set device to sleep for specific duration (duration in milliseconds)
     def sleep(self, duration):
-        return(self.execute(self.COMMANDS["SYSTEM"]["SLEEP"].format(duration=str(duration))))
-    
+        ''' Sets device to sleep '''
+        return self.execute(self.COMMANDS["SYSTEM"]["SLEEP"].format(duration=str(duration)))
     # Set device to reset (ON OFF)
     def reset(self):
-        return(self.execute(self.COMMANDS["SYSTEM"]["RESET"]))
-    
+        ''' Resets RN2483 '''
+        return self.execute(self.COMMANDS["SYSTEM"]["RESET"])
     # Factory reset device
-    def factoryReset(self):
-        return(self.execute(self.COMMANDS["SYSTEM"]["FACTORY_RESET"]))
-    
+    def factory_reset(self):
+        ''' Factory resets RN2483 '''
+        return self.execute(self.COMMANDS["SYSTEM"]["FACTORY_RESET"])
     # Change state of GPIO pin: 1 = UP, 0 = DOWN
-    def setPin(self, pin, state):
-        return(self.execute(self.COMMANDS["SYSTEM"]["PIN"][str(pin)].format(state=str(state))))
-        
-    # Setup LoRaWAN client     
-    def configLoraWAN(self, auth=None, nwkskey=None, appskey=None, appkey=None, appeui=None, devaddr=None, power=14):
-        if self.auth == "ABP":
+    def set_pin(self, pin, state):
+        ''' Sets state of GPIO pin '''
+        return self.execute(self.COMMANDS["SYSTEM"]["PIN"][str(pin)].format(state=str(state)))
+    # Setup LoRaWAN client
+    def config_lorawan(self, auth=None, nwkskey=None, appskey=None, appkey=None, appeui=None, devaddr=None, power=14):
+        ''' Configures LoRaWAN functionlity to specified autorization method '''
+        if auth == "ABP" or auth == "abp":
             if self.debug:
-                print("Initializing LoRaWAN - Authorization = ABP")
+                print "Initializing LoRaWAN - Authorization = ABP"
             self.execute(self.COMMANDS["SYSTEM"]["HWEUI"])
-            
             if self.debug:
-                print("Configuring Network Session Key - Key: {nwkskey}".format(nwkskey=nwkskey))
+                print "Configuring Network Session Key - Key: {nwkskey}".format(nwkskey=nwkskey)
             self.execute(self.COMMANDS["MAC"]["NWKSKEY"].format(key=nwkskey))
-            
             if self.debug:
-                print("Configuring Application Session Key - Key: {appskey}".format(appskey=appskey))
+                print "Configuring Application Session Key - Key: {appskey}".format(appskey=appskey)
             self.execute(self.COMMANDS["MAC"]["APPSKEY"].format(key=appskey))
-
             if self.debug:
-                print("Configuring Device Address - Address: {devaddr}".format(devaddr=devaddr))
+                print "Configuring Device Address - Address: {devaddr}".format(devaddr=devaddr)
             self.execute(self.COMMANDS["MAC"]["SET"]["DEVADDR"].format(address=devaddr))
-            
             if self.debug:
-                print("Configuring Adaptive Data Rate")
+                print "Configuring Adaptive Data Rate"
             self.execute(self.COMMANDS["MAC"]["SET"]["ADAPTIVE_DATARATE"].format(state="on"))
-
             if self.debug:
-                print("Setting transmit power to {power}".format(power=str(power))
+                print "Setting transmit power to {power}".format(power=str(power))
             self.execute(self.COMMANDS["RADIO"]["POWER"]["SET"].format(level=str(power)))
-
             if self.debug:
-                print("Saving configuration")
+                print "Saving configuration"
             self.execute(self.COMMANDS["MAC"]["SAVE"])
-
             if self.debug:
-                print("Joining...")
+                print "Joining..."
             self.execute(self.COMMANDS["MAC"]["JOIN"].format(mode="abp"))
-
-        elif self.auth == "OTAA" or self.auth == "otaa":
+        elif auth == "OTAA" or auth == "otaa":
             if self.debug:
-                print("Initializing LoRaWAN - Authorization = OTAA")
+                print "Initializing LoRaWAN - Authorization = OTAA"
             self.execute(self.COMMANDS["SYSTEM"]["HWEUI"])
-            
             if self.debug:
-                print("Configuring Application Key - Key: {appkey}".format(appkey=appkey))
+                print "Configuring Application Key - Key: {appkey}".format(appkey=appkey)
             self.execute(self.COMMANDS["MAC"]["APPKEY"].format(key=appkey))
-            
             if self.debug:
-                print("Configuring Application EUI - Key: {appeui}".format(appeui=appeui))
+                print "Configuring Application EUI - Key: {appeui}".format(appeui=appeui)
             self.execute(self.COMMANDS["MAC"]["SET"]["APPEUI"].format(appeui=appeui))
-
             if self.debug:
-                print("Saving configuration")
+                print "Saving configuration"
             self.execute(self.COMMANDS["MAC"]["SAVE"])
-
             if self.debug:
-                print("Joining...")
+                print "Joining..."
             self.execute(self.COMMANDS["MAC"]["JOIN"].format(mode="otaa"))
-
         else:
-            print("Error with LoRaWAN configuration")
-
-    
-            
-
-    
+            print "Error with LoRaWAN configuration"
 def main():
+    ''' Main function '''
     uart = serial.Serial(PORT, BAUDRATE)
-
     device = RN2483(serial=uart, debug=True)
-    print(device)
-
-
-    
+    print device
     uart.close()
-    
 if __name__ == "__main__":
     main()
     
